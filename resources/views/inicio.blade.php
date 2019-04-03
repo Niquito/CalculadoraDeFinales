752b17f859a841ecc94a0c72380e10a9411c7294
H. Nicolás Caffarini <ncaffarini@gmail.com>
2019-04-03 10:56:39 -0300

    Añadida base de TyC

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -87,7 +87,108 @@
         </button>
       </div>
       <div class="modal-body">
-        Y acá es donde empieza el bardo
+        <p>TERMINOS Y CONDICIONES</p>
+<p>
+Los presentes términos y condiciones  (los "Términos y Condiciones" o "T&C") regulan la relación contractual entre los usuarios, definiendo como tales a las personas humanas o jurídicas que se encuentren autorizadas al uso de las Plataformas, conforme se define más adelante (en adelante, los “Usuarios”), con el estudiante desarrollador de la plataforma (en adelante, "Nico"), RN: 889222, con domicilio de su facultad en Av. Córdoba 2122 (Aula 302), Buenos Aires, Capital Federal, C.P. 1120 AAQ, dirección de e-mail ncaffarini@gmail.com (en adelante Nico junto con los Usuarios, las “Partes”). Los Usuarios se encontrarán sujetos a los Términos y Condiciones generales respectivos, junto con todas las demás políticas y principios que rige Nico y que son incorporados al presente por referencia.
+CUALQUIER PERSONA QUE NO ACEPTE ESTOS TÉRMINOS Y CONDICIONES GENERALES, LOS CUALES TIENEN UN CARÁCTER OBLIGATORIO Y VINCULANTE, DEBERÁ ABSTENERSE DE UTILIZAR LOS SERVICIOS, CONFORME SON DEFINIDOS MÁS ADELANTE.
+El Usuario debe leer, entender y aceptar todas las condiciones establecidas en los Términos y Condiciones generales, que incluyen a su vez las Políticas de Privacidad.
+Nico es un estudiante debidamente inscripto en la Facultad de Ciencias Económicas de la Universidad de Buenos Aires, y tiene como objeto el estudio de los resultados arrojados por la plataforma sin ningún tipo de fin lucrativo. Nico provee una plataforma web ubicada en la dirección actual de su navegador y un espacio de dominio delegado en Hostinger (en adelante, conjuntamente, las “Plataformas”) con el fin de promocionar a los Usuarios sobre los productos y servicios ofrecidos por Nico (en adelante, los “Servicios”).</p>
+
+
+
+<p>CLAUSULA PRIMERA: Condiciones Generales de los Servicios</p>
+<p>
+1.1. Para el acceso a los contenidos de los Servicios no será necesario el registro del Usuario. No obstante ello, en el supuesto en que para acceder a determinados Servicios prestados por Nico, el Usuario deba completar con sus datos personales, estos deberán ser datos válidos y verificar que la información que pone a disposición de Nico sea exacta, precisa y verdadera (en adelante, los "Datos Personales"). Los Usuarios garantizan y responden, en cualquier caso, de la veracidad, exactitud, vigencia y autenticidad de los Datos Personales puestos a disposición de Nico.
+</p>
+<p>
+1.2. Queda prohibido que un competidor acceda a los Servicios prestados por Nico, salvo consentimiento previo de Nico. También queda prohibido acceder a los Servicios prestados por Nico con el fin de monitorear su desempeño o funcionalidad, publicándolos o haciéndolos accesibles a cualquier competidor o tercero no regido por estos Términos y Condiciones, o para cualquier otro punto de referencia o propósitos competitivos.
+</p>
+<p>
+1.3. Los Servicios consisten en la promoción a los Usuarios sobre los productos y servicios ofrecidos por Nico, encontrándose las Plataformas destinadas a tal fin. 
+</p>
+<p>
+1.4. La adquisición de cualquier producto o servicio ofrecido por Nico se regirá específicamente por los términos de la correspondiente legislación argentina vigente de contrato aplicable y no por los Términos y Condiciones de las Plataformas.
+</p>
+<p>
+1.5. La información que se brinda a través de las Plataformas tiene carácter meramente informativo y no constituye una oferta de venta ni una búsqueda de ofertas de compra de ningún producto o servicio a que se pudiera hacer referencia en las mismas. De igual modo, los cálculos de cualquier producto o servicio a través de las Plataformas se realiza mediante un mecanismo de "simulación" de un cálculo y no constituye una oferta concreta de ningún producto y/o servicio.
+</p>
+<p>
+1.6. Los Servicios de Nico promocionados a través de las Plataformas son ofrecidos para su contratación dentro de la República Argentina por lo que los contratantes que se encuentren fuera de dicho territorio deberán constatar previamente la existencia de eventuales limitaciones legales para la adquisición de los mismos. 
+</p>
+<p>
+1.7. El acceso y utilización de las Plataformas no posee limitación temporal alguna. Sin perjuicio de ello, Nico podrá dar por terminado dicho acceso en cualquier momento sin expresión de causa y sin que ello genere derecho alguno a favor de los Usuarios CLAUSULA SEGUNDA. 
+</p>
+<p>
+Uso de las Plataformas y/o de los Servicios.
+</p>
+<p>
+2.1.El acceso y la utilización de las Plataformas es de carácter gratuito y por tiempo indeterminado, sujeto al estricto cumplimiento por parte de los Usuarios de los presentes Términos y Condiciones, así como el respeto por la legislación vigente.
+</p>
+<p>
+2.2.El Usuario se compromete a hacer un uso adecuado y lícito de las Plataformas de conformidad con la legislación aplicable, los presentes Términos y Condiciones, la moral y buenas costumbres generalmente aceptadas y al orden público.
+</p>
+<p>
+2.3. Al utilizar las Plataformas y/o los Servicios, el Usuario acuerda que:
+</p>
+<p>
+a. No solicitará las Plataformas y/o el Servicio con fines ilícitos, ilegales, contrarios a lo establecido en los presentes Términos y Condiciones, a la buena fe y al orden público, lesivos de los derechos e intereses de terceros.
+b. No tratará de dañar las Plataformas y/o los Servicios de ningún modo, ni accederá a recursos restringidos en las Plataformas y/o los Servicios.
+c. No utilizará las Plataformas y/o los Servicios con un dispositivo incompatible o no autorizado.
+d. No introducirá ni difundirá virus informáticos o cualesquiera otros sistemas físicos o lógicos que sean susceptibles de provocar daños en las Plataformas y/o los Servicios.
+</p>
+
+<p>CLAUSULA TERCERA: Garantía de las Plataformas y/o los Servicios.</p>
+<p>
+3.1. El Usuario accederá y utilizará las Plataformas por decisión voluntaria y bajo su exclusiva responsabilidad. Nico no puede garantizar la disponibilidad y/o el funcionamiento continuo e ininterrumpido de las Plataformas. De igual modo, Nico no garantiza la utilidad de las Plataformas para la realización de ninguna actividad en particular, ni su infalibilidad, ni la adecuación de la información contenida en el mismo para fines específicos ya que la misma posee carácter meramente informativo y la contratación de cualquier producto y/o servicio determinado requiere del asesoramiento e intervención personal de un asesor de MS.
+</p>
+<p>
+3.2. Nico desde ya se deslinda de todo tipo de responsabilidad legal por los daños y perjuicios de cualquier naturaleza que puedan derivarse de la falta de disponibilidad o de la falta de funcionamiento ininterrumpido de las Plataformas y/o los Servicios  o contenidos. Asimismo, Nico no otorga ninguna garantía ni se hace responsable, en ningún caso, de los daños y perjuicios de cualquier naturaleza que se deriven de la falta de utilidad, adecuación o validez de las Plataformas y/o los Servicios  para satisfacer necesidades, actividades o resultados concretos o expectativas de los Usuarios. Nico no asume por la puesta a disposición de las Plataformas el carácter de asesor legal, financiero, impositivo o de cualquier otra clase. Nico no asume responsabilidad alguna por el uso ilícito y/o contrario a los presentes Términos y Condiciones.
+</p>
+<p>
+3.3. Nico no garantiza la ausencia de virus ni de otros elementos en las Plataformas introducidos por terceros ajenos a Nico que puedan producir alteraciones en los sistemas físicos o lógicos del Usuario o en los documentos electrónicos y archivos almacenados en sus sistemas. En consecuencia, Nico no será en ningún caso responsable de cualesquiera daños y perjuicios de toda naturaleza que pudieran derivarse de la presencia de virus u otros elementos que puedan producir alteraciones en los sistemas físicos o lógicos, documentos electrónicos o archivos del Usuario.
+</p>
+<p>
+3.4. Nico adopta diversas medidas de protección para proteger las Plataformas y los contenidos contra ataques informáticos de terceros. No obstante, Nico no garantiza que terceros no autorizados no puedan conocer las condiciones, características y circunstancias en las cuales el Usuario accede a las Plataformas. En consecuencia, Nico no será en ningún caso responsable de los daños y perjuicios que pudieran derivarse de dicho acceso no autorizado. 
+</p>
+<p>
+3.5. Con la aceptación de los presentes Términos y Condiciones, el Usuario declara que mantendrá indemne frente a cualquier reclamo a Nico, sus empresas controladas y controlantes, directores, socios, empleados, abogados y agentes, derivado del (i) incumplimiento por parte de los Usuarios de cualquier disposición contenida los presentes Términos y Condiciones o de cualquier ley o regulación aplicable a las mismas, (ii) incumplimiento o violación de los derechos de terceros incluyendo, a título meramente enunciativo, otros Usuarios; y (iii) incumplimiento del uso permitido de las Plataformas y/o los Servicios.
+</p>
+<p>
+3.6. Los archivos que contiene el sitio web tienen carácter estimativo, indicativo y orientativo en forma exclusiva. Nico no garantiza que todos los contenidos de las Plataformas y los archivos: (i) se encontrarán actualizados y siempre disponibles; (ii) estarán libres de error y que los errores serán corregidos; y (iii) se encontrarán libres de virus u otro componente dañino. 
+</p>
+<p>
+3.7.Las Plataformas pueden tener hipervínculos con otros sitios web, los cuales Nico no edita, controla, mantiene o supervisa de forma alguna, no siendo responsable por tanto, por el contenido de dichos sitios web. El contenido de dichos sitios web es responsabilidad de su titular, y Nico no avala o aprueba dichos contenidos. El icono que contiene el hipervínculo de la dirección del sitio web de Nico, tiene el único objeto de proveer información sobre la corporación y su estructura en Argentina. 
+</p>
+
+<p>CLAUSULA CUARTA: Derechos de Propiedad Intelectual e Industrial</p>
+<p>
+4.1. El Usuario, reconoce y acepta que todos los derechos de propiedad intelectual e industrial sobre los contenidos y/o cualesquiera otros elementos insertados en las Plataformas (incluyendo, sin limitación los diseños, marcas, nombres comerciales, textos, denominaciones, imágenes, logos, logotipos, gráficos, iconos, nombres de dominio, sonidos, bases de datos, software, diagramas de flujo, presentación, audio y vídeo) pertenecen a Nico. y. Cualquier copia o uso no autorizado del diseño o contenido que difiera del expresamente permitido por Nico, en su carácter de titular de los derechos de propiedad intelectual, afectará los derechos del mencionado titular conforme los tratados internacionales y la normativa aplicable y vigente de la República Argentina. 
+</p>
+<p>
+4.2. Nico autoriza al Usuario, a utilizar, visualizar, imprimir, descargar y almacenar los contenidos y/o los elementos insertados en las Plataformas exclusivamente para su uso personal, privado y no lucrativo, absteniéndose de realizar sobre los mismos cualquier acto de descompilación, ingeniería inversa, modificación, divulgación o suministro. Cualquier otro uso o explotación de cualesquiera contenidos y/u otros elementos insertados en las Plataformas distinto de los aquí expresamente previstos estará sujeto a la autorización previa de Nico. 
+</p>
+
+<p>CLAUSULA QUINTA: Modificaciones.</p>
+<p>
+5.1. Nico podrá modificar en cualquier momento los Términos y Condiciones de las Plataformas. Si Nico decidiera cambiar los mismos, serán publicados e informados a través de las mismas y/o a través de una comunicación por correo electrónico, por lo que el Usuario deberá realizar una revisión periódica de las Plataformas y su correo electrónico a fin de verificar los Términos y Condiciones vigentes al momento. La continuación en el uso de las Plataformas implica la aceptación por parte del Usuario de las modificaciones introducidas.
+</p>
+
+<p>CLAUSULA SEXTA: Protección de Datos</p>
+<p>
+6.1. En aquellos casos en que los Usuarios proporcionen Datos Personales, los mismos se integrarán en una base de datos personales de la que es responsable Nico.
+</p>
+<p>
+6.2. Nico declara que los Datos Personales de los Usuarios, serán utilizados únicamente con el fin mencionado en la CLAUSULA PRIMERA. 
+</p>
+<p>6.3. Los Usuarios podrán ejercitar los derechos de acceder, rectificar, suprimir y actualizar su Información Personal, así como a oponerse al tratamiento de la misma, todo ello de conformidad a lo dispuesto en la normativa aplicable (Ley 25.326 - Ley de Protección de Datos Personales).
+</p>
+<p>
+6.4. Nico, se compromete a garantizar el cumplimiento de lo establecido en la ley 25.326 y el decreto 1558/2001 respecto de la Protección de los Datos Personales, de los Usuarios y/o de cualquier tercero, así como de garantizar los derechos conferidos en la normativa mencionada.
+</p>
+
+<p>CLAUSULA SÉPTIMA: Ley Aplicable y Jurisdicción</p>
+<p>
+7.1. Cualquier controversia o conflicto que pueda surgir como consecuencia del uso de las Plataformas será resuelto de acuerdo con la legislación vigente en la República Argentina y deberá someterse obligatoriamente a la jurisdicción y competencia de los Tribunales Ordinarios de la Ciudad de Buenos Aires (República Argentina) renunciando expresamente a cualquier otro fuero o jurisdicción que pudiere corresponder.</p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Cerrar</button>
